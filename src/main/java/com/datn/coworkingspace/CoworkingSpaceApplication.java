4e03b8f3240ae9aa20cb32c1a22a24a9fa311edb
pnhoa <hoapham1232000@gmail.com>
2022-09-17 10:25:11 +0700

    init project

---
+++ src/main/java/com/datn/coworkingspace/CoworkingSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.datn.coworkingspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datn/coworkingspace/CoworkingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.coworkingspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
